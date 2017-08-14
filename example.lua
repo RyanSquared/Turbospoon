2e01059a601d0c445fb0af6eb1496c642b31cabc
RyanSquared <vandor2012@gmail.com>
2017-08-14 03:04:06 -0500

    example: Remove

--- example.lua
+++
@@ -1,8 +0,0 @@
-local App
-App = require("tbsp").App
-local app = App({
-  debug_level = 5
-})
-app:bind("::", "8000")
-app:bind("::", "8001")
-return print(app:run())
--- example.moon
+++
@@ -1,8 +0,0 @@
-import App from require "tbsp"
-
-app = App debug_level: 5
-
-app\bind "::", "8000"
-app\bind "::", "8001"
-
-print app\run!
